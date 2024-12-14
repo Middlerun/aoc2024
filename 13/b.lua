149cac6ff63d256f47c6df20d4745b043f30feab
Eddie McLean <middlerun@gmail.com>
2024-12-14 16:43:34 +1100

    Simplify

--- 13/b.lua
+++ 13/b.lua
@@ -6,36 +6,27 @@ require('13/common')
 local function getSolution(machine)
   local am = machine.ay / machine.ax -- gradient of line A
   local bm = machine.by / machine.bx -- gradient of line B
-  local pm = machine.py / machine.px
 
   if am == bm then
     print('Special case! Lines have same gradient. Handle this if it comes up')
     os.exit()
   end
 
-  local bi = machine.py - bm * machine.px -- y-intercept of line B
-
   -- Find point where lines intersect
+  local bi = machine.py - bm * machine.px -- y-intercept of line B
   local ix = bi / (am - bm)
-  local iy = am * ix
 
-  -- See if this allows an integer number of button presses for A and B
-  local aPresses, fractionalAPresses = math.modf(ix / machine.ax)
-  local bPresses, fractionalBPresses = math.modf((machine.px - ix) / machine.bx)
+  -- Round to account for floating point errors
+  -- Then we'll verify below if it's actually valid
+  local aPresses = round(ix / machine.ax)
+  local bPresses = round((machine.px - ix) / machine.bx)
 
-  -- Account for floating point errors by checking if the fractional parts are small
-  -- instead of checking if it's zero
-  if (fractionalAPresses < 0.01 or fractionalAPresses > 0.99) and (fractionalBPresses < 0.01 or fractionalBPresses > 0.99) then
-    aPresses = round(ix / machine.ax)
-    bPresses = round((machine.px - ix) / machine.bx)
-
-    if aPresses >= 0 and bPresses >= 0 then
-      -- Verify
-      local finalX = aPresses * machine.ax + bPresses * machine.bx
-      local finalY = aPresses * machine.ay + bPresses * machine.by
-      if finalX == machine.px and finalY == machine.py then
-        return { aPresses = aPresses, bPresses = bPresses }
-      end
+  if aPresses >= 0 and bPresses >= 0 then
+    -- Verify
+    local finalX = aPresses * machine.ax + bPresses * machine.bx
+    local finalY = aPresses * machine.ay + bPresses * machine.by
+    if finalX == machine.px and finalY == machine.py then
+      return { aPresses = aPresses, bPresses = bPresses }
     end
   end
 end
